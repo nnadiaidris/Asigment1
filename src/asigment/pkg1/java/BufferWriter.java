1b5a6ce4e6d6637e88220e09642d13351c148c90
nnadiaidris <n.nadiaidris@gmail.com>
2015-10-06 06:10:42 +0800

    new commit

---
+++ src/asigment/pkg1/java/BufferWriter.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asigment.pkg1.java;
+
+import java.io.FileWriter;
+
+/**
+ *
+ * @author Nurul.Nadia.Idris
+ */
+class BufferWriter {
+
+    BufferWriter(FileWriter fileWriter) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void write(String this_is_line_one) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void newLine() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
